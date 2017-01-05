b8f931306437a70ada6196c5317b79649bf2cc43
Jacek Witkowski <jacek.witkowski@gmail.com>
2017-01-05 13:31:38 +0100

    Added main script for testing the CifarReader (generates data which are later visualized using TensorBoard).

---
+++ src/main.py
@@ -0,0 +1,43 @@
+import logging
+import os
+import sys
+
+import tensorflow as tf
+
+from cifar_reader import CifarReader
+
+TMP_DIR = os.path.join('..', 'tmp')
+
+root = logging.getLogger()
+root.setLevel(logging.DEBUG)
+
+ch = logging.StreamHandler(sys.stdout)
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s [%(levelname)s]\t %(message)s')
+ch.setFormatter(formatter)
+root.addHandler(ch)
+
+BATCH_SIZE = 1000
+LOG_DIR = os.path.join(TMP_DIR, 'summary')
+MAX_STEPS = 10
+
+# with tf.Session() as sess:
+#     summary_writer = tf.train.SummaryWriter("summary", graph_def=sess.graph_def)
+reader = CifarReader(data_dir=TMP_DIR)
+reader.download_dataset_if_necessary()
+
+if tf.gfile.Exists(LOG_DIR):
+    tf.gfile.DeleteRecursively(LOG_DIR)
+tf.gfile.MakeDirs(LOG_DIR)
+
+with tf.Session(config=tf.ConfigProto(log_device_placement=False)).as_default() as sess:
+    images, labels = reader.load_dataset(batch_size=BATCH_SIZE, use_train_data=False, distort_image=False)
+    init_op = tf.global_variables_initializer()
+    summary_op = tf.summary.merge_all()
+    tf.train.start_queue_runners(sess=sess)
+
+    summary_writer = tf.summary.FileWriter(logdir=LOG_DIR, graph=sess.graph)
+    sess.run(init_op)
+    summary_result = sess.run(summary_op)
+    summary_writer.add_summary(summary_result)
+    summary_writer.close()
