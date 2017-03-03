6f6be4f39c30b5e332e87919d1ea23be0c2b83fe
Jacek Witkowski <jacek.witkowski@gmail.com>
2017-03-03 16:14:23 +0100

    Used only keyword arguments in tf.nn.sparse_softmax_cross_entropy_with_logits as required by a new version of tensorflow.

--- src/training.py
+++ src/training.py
@@ -16,7 +16,8 @@ class NetTrainer(object):
 
     def loss(self, logits, labels):
         labels = tf.cast(labels, tf.int64)
-        cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits, labels, name='cross_entropy_per_example')
+        cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=labels,
+                                                                       name='cross_entropy_per_example')
         cross_entropy_mean = tf.reduce_mean(cross_entropy, name='cross_entropy')
         tf.add_to_collection('losses', cross_entropy_mean)
 
